43bd039c472186cd1869f7724b858c2d38a14020
paulina zulen <pzulen28@gmail.com>
2022-01-22 14:34:26 -0600

    agregue el enlace de facultades al modulo de documentacion

--- administrador/docu.php
+++ administrador/docu.php
@@ -40,6 +40,9 @@ if(isset($_SESSION['login'])){
             <a class="nav-link text-white menu-item p-2 text-center" href="#">Documentación</a>
           </li>
           <li class="nav-item">
+            <a class="nav-link text-white menu-item p-2 text-center" href="facultades.php">Facultades</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link text-white menu-item p-2 text-center" href="../ip.php">Datos Conexión</a>
           </li>
           <li class="nav-item">
